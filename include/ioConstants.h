a9a428c75cd6b3de1be36b5a8728bb55e7628e69
mila <michael.lange14@freenet.de>
2022-04-03 14:40:52 +0200

    added water support

---
+++ include/ioConstants.h
@@ -0,0 +1,14 @@
+#define BUTTON_PRESSED LOW
+
+#define BUTTON_1 2 //define button pin D2
+#define BUTTON_2 3 //define button pin D3
+
+#define MOTION 7 // define motion to pin D1
+
+#define BUZZER 11 // define the buzzer with pin D11
+
+#define WATER_LEVEL 10
+
+#define MOTOR_1 4 // yellow
+#define MOTOR_2 5 // green
+#define MOTOR_3 6 // white
